6133fe124181a28ca3f3d76d752ff772f64eed39
kle160 <lekevin2013@gmail.com>
2022-04-23 11:52:55 -0700

    created signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './index.css';
+import SignInOutContainer from '.components/SignInOutContainer.jsx';
+
 
 function App() {
-  return <div className="App">CrashCode</div>;
+  return (
+    <div className='App'>
+        <SignInOutContainer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/login.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox'
+import icon from '../images/codesmith.png'
+
+const Login=({handleChange})=>{
+
+    const paperStyle={padding :20,height:450,width:300, margin:"0 auto"}
+    const btnstyle={margin:'8px 0'}
+    return(
+        <Grid>
+            <Paper style={paperStyle}>
+                <Grid align='center'>
+                     <img src={icon}></img>
+                    <h1>CRASH COURSE</h1>
+                    <h2>Sign In</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter username' fullWidth required/>
+                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
+                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                <Typography align='center'> <Link href="#" > Forgot password </Link>
+                </Typography>
+                <Typography align='center'> Do you have an account?
+                    <Link href="#" onClick={()=>handleChange("event",1)}> Sign Up</Link>
+                </Typography>
+            </Paper>
+        </Grid>
+    )
+}
+
+export default Login;
