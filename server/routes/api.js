6545749d6aa6eb45247cb07ea2d0834ff027707a
Em <epithe@gmail.com>
2022-04-23 12:39:07 -0400

    create user, get cards, and create card functionality

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,9 +6,20 @@ const crashcodeController = require('../controllers/crashcodeController');
 
 // controllers go here
 
-// getCards - '/cards'
+// addUser - '/users
+router.post('/users', crashcodeController.addUser, (req, res) => {
+    res.status(200).json(res.locals.userId);
+})
+
+// getCards - '/cards' // should take in user as parameter
+router.get('/cards', crashcodeController.getCards, (req, res) => {
+    res.status(200).json(res.locals.cards);
+})
 
 // createCards - '/cards'
+router.post('/cards', crashcodeController.createCard, crashcodeController.addCardToUser, crashcodeController.getCards, (req, res,) => {
+    res.status(200).json(res.locals.cards);
+})
 
 // updateCard - '/cards/:id'
 
